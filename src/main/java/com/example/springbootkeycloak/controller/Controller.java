2c47863855068f6f1d66562427bb18c1bef123d2
Bryan Danquah <brayndanquah@gmail.com>
2024-05-01 21:38:28 +0000

    JWT setups, Security Config

--- src/main/java/com/example/springbootkeycloak/controller/Controller.java
+++ src/main/java/com/example/springbootkeycloak/controller/Controller.java
@@ -1,13 +1,12 @@
 package com.example.springbootkeycloak.controller;
 
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api")
 public class Controller {
 
     @GetMapping("/hello")
